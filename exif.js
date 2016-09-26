26d8ddb0c02f942b8221968e7861612e6cd27b7b
Stephen Eduardo Camilo <stvcomp@gmail.com>
2016-09-26 11:28:28 -0300

    Reading files from a folder of ariginals in order to save the watermarked images in another

--- exif.js
+++ exif.js
@@ -1,14 +1,26 @@
 var ExifImage = require('exif').ExifImage;
 var watermark = require('image-watermark');
+var fs = require('fs');
 
-new ExifImage({ image : 'myImage.jpg' }, function (error, exifData) {
-	console.log(exifData);
+var files = fs.readdirSync('originals');
+
+for (var file in files) {
+	file = files[file];
+	try {
+		var img = new ExifImage({ image : 'originals/'+file }, function (error, exifData) {
+			if (error) console.log('Error: '+error.message);
+			else var date = exifData.exif['DateTimeOriginal']; // Do something with your data!
+		});
+	} catch (error) {
+		console.log('Error: ' + error.message);
+	}
+
 	var options = {
-		'text' : exifData.exif['DateTimeOriginal'],
-		'dstPath' : 'watermark.jpg',
+		'text' : img.date,
+		'dstPath' : 'watermarked/'+'dated_'+file,
 		'color' : 'rgb(177, 177, 177)',
 		'align' : 'ltr',
 		'position':'Left'
 	};
-	watermark.embedWatermark('myImage.jpg', options);
-});
+	watermark.embedWatermark('originals/'+file, options);
+}
